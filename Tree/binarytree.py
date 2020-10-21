8db2affd4bba0ae7f3d9d271a88e6ff5db9b11e0
Tbetcha <troy.boettger@gmail.com>
2020-10-21 01:46:07 -0400

    implement in-order and pre-order traversal

--- Tree/binarytree.py
+++ Tree/binarytree.py
@@ -4,13 +4,21 @@ class TreeNode:
         self.left_child = None
         self.right_child = None
 
-
+# dfs
 def pre_order(root_node):
     if not root_node:
         return
     print(root_node.data)
     pre_order(root_node.left_child)
     pre_order(root_node.right_child)
+
+#dfs
+def in_order(root_node):
+    if not root_node:
+        return
+    in_order(root_node.left_child)
+    print(root_node.data)
+    in_order(root_node.right_child)
 
 
 new_tree = TreeNode('Drinks')
@@ -19,3 +27,4 @@ right_child = TreeNode("Cold")
 new_tree.left_child = left_child
 new_tree.right_child = right_child
 pre_order(new_tree)
+in_order(new_tree)
