bd7dabcc6d749f4d08f284536e72a6aba7d6e9cc
TMB <Troy.Boettger@gmail.com>
2020-10-25 04:13:01 -0400

    implement queue as linked list

--- LinearDS/queue.py
+++ LinearDS/queue.py
@@ -4,7 +4,7 @@ class Queue:
 
     def __str__(self):
         if self.is_empty():
-            return "The queue is empty"
+            return "The Queue is empty"
         else:
             values = [str(x) for x in self.items]
             return ' '.join(values)
@@ -21,19 +21,19 @@ class Queue:
 
     def deq(self):
         if self.is_empty():
-            return "The queue is empty"
+            return "The Queue is empty"
         else:
             return self.items.pop(0)
 
     def peek(self):
         if self.is_empty():
-            return "The queue is empty"
+            return "The Queue is empty"
         else:
             return self.items[0]
 
-
     def delete(self):
         self.items = None
+
 
 qu = Queue()
 print(qu.is_empty())
--- Tree/binarytree.py
+++ Tree/binarytree.py
@@ -4,6 +4,7 @@ class TreeNode:
         self.left_child = None
         self.right_child = None
 
+
 # dfs
 def pre_order(root_node):
     if not root_node:
@@ -12,7 +13,8 @@ def pre_order(root_node):
     pre_order(root_node.left_child)
     pre_order(root_node.right_child)
 
-#dfs
+
+# dfs
 def in_order(root_node):
     if not root_node:
         return
@@ -21,10 +23,26 @@ def in_order(root_node):
     in_order(root_node.right_child)
 
 
+def post_order(root_node):
+    if not root_node:
+        return
+    post_order(root_node.left_child)
+    pre_order(root_node.right_child)
+    print(root_node.data)
+
+
+
+
 new_tree = TreeNode('Drinks')
 left_child = TreeNode("Hot")
 right_child = TreeNode("Cold")
+tea = TreeNode("Tea")
+coffee = TreeNode("Coffee")
+left_child.left_child = tea
+left_child.right_child = coffee
 new_tree.left_child = left_child
 new_tree.right_child = right_child
-pre_order(new_tree)
-in_order(new_tree)
+# pre_order(new_tree)
+# in_order(new_tree)
+# post_order(new_tree)
+# level_order(new_tree)
