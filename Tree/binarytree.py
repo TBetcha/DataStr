c82e2eb07ffaee3b9cf90ebb371be6f085409e76
Tbetcha <troy.boettger@gmail.com>
2020-11-01 10:43:16 -0500

    implement deleting from binary tree

--- Tree/binarytree.py
+++ Tree/binarytree.py
@@ -87,6 +87,66 @@ def insert(root_node, new_node):
                 return "successfully inserted"
 
 
+def get_deepest(rootNode):
+    if not rootNode:
+        return
+    else:
+        cust = linkedQueue.Queue()
+        cust.enqueue(rootNode)
+        while not (cust.is_empty()):
+            root = cust.dequeue()
+            if root.value.left_child is not None:
+                cust.enqueue(root.value.left_child)
+            if root.value.right_child is not None:
+                cust.enqueue(root.value.right_child)
+        deepest = root.value
+        return deepest
+
+
+def del_deepest(root_node, d_node):
+    if not root_node:
+        return
+    else:
+        cust = linkedQueue.Queue()
+        cust.enqueue(root_node)
+        while not (cust.is_empty()):
+            root = cust.dequeue()
+            if root.value is d_node:
+                root.value = None
+            if root.value.right_child:
+                if root.value.right_child is d_node:
+                    root.value.right_child = None
+                    return
+                else:
+                    cust.enqueue(root.value.right_child)
+            if root.value.left_child:
+                if root.value.left_child is d_node:
+                    root.value.left_child = None
+                    return
+                else:
+                    cust.enqueue(root.value.left_child)
+
+
+def delete_node(root_node, node):
+    if not root_node:
+        return "The Binary Tree does not exist"
+    else:
+        cust = linkedQueue.Queue()
+        cust.enqueue(root_node)
+        while not (cust.is_empty()):
+            root = cust.dequeue()
+            if root.value.data == node:
+                d_node = get_deepest(root_node)
+                root.value.data = d_node.data
+                del_deepest(root_node, d_node)
+                return "The node has been deleted, y'all!"
+            if root.value.left_child is not None:
+                cust.enqueue(root.value.left_child)
+            if root.value.right_child is not None:
+                cust.enqueue(root.value.right_child)
+        return "Failed to delete"
+
+
 new_tree = TreeNode('Drinks')
 left_child = TreeNode("Hot")
 right_child = TreeNode("Cold")
@@ -100,7 +160,18 @@ new_tree.right_child = right_child
 # in_order(new_tree)
 # post_order(new_tree)
 # level_order(new_tree)
-print(search(new_tree, "Tea"))
+# print(search(new_tree, "Tea"))
 new_node = TreeNode("Cola")
 insert(new_tree, new_node)
+level_order(new_tree)
+deepest_node = get_deepest(new_tree)
+print('----')
+print(deepest_node.data)
+newnode = get_deepest(new_tree)
+del_deepest(new_tree, newnode)
+print('level-----------')
+level_order(new_tree)
+print('level-----------')
+delete_node(new_tree, 'Hot')
+print('level-----------')
 level_order(new_tree)
