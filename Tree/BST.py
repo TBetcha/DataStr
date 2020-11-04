6197367085eab92ebdaed246686e819475d0fe72
TMB <Troy.Boettger@gmail.com>
2020-11-03 19:56:06 -0500

    add traversals for bst

--- Tree/BST.py
+++ Tree/BST.py
@@ -21,7 +21,35 @@ def insert_node(root_node, node_value):
     return "The node has been inserted"
 
 
+
+def pre_order(root_node):
+    if not root_node:
+        return 
+    print(root_node.data)
+    pre_order(root_node.left_child)
+    pre_order(root_node.right_child)
+
+
+def in_order(root_node):
+    if not root_node:
+        return
+    in_order(root_node.left_child)
+    print(root_node.data)
+    in_order(root_node.right_child)
+
+def post_order(root_node):
+    if not root_node:
+        return 
+    post_order(root_node.left_child)
+    post_order(root_node.right_child)
+    print(root_node.data)
+
 new_tree = Node(None)
 print(insert_node(new_tree, 70))
 print(insert_node(new_tree, 60))
-
+print(insert_node(new_tree, 90))
+print(insert_node(new_tree, 20))
+print(insert_node(new_tree, 80))
+print(insert_node(new_tree, 10))
+print(insert_node(new_tree, 50))
+print(post_order(new_tree))
