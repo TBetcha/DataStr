98e22a903b95d47f30d15acd76de3da94af9bc05
TMB <Troy.Boettger@gmail.com>
2021-03-19 06:06:15 -0400

    trie insert method

--- Tree/trie.py
+++ Tree/trie.py
@@ -10,15 +10,29 @@ every node keeps track of the 'end of string'
 '''
 
 
-class Trie_node:
+class TrieNode:
     def __init__(self):
         self.children = {}
         self.end_string = False
 
 
 class Trie:
-    def __init(self):
-        self.root = Trie_node()
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert_string(self, word):
+        current = self.root
+        for i in word:
+            ch = i
+            node = current.children.get(ch)
+            if node == None:
+                node = TrieNode()
+                current.children.update({ch: node})
+            current = node
+        current.end_string = True
+        print("Successfully inserted")
 
 
 trie_one = Trie()
+trie_one.insert_string('App')
+trie_one.insert_string("Appl")
