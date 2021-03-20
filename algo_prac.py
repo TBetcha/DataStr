f06adc0f9aa77e7c78ae3a2235527b0c55f211b1
TMB <Troy.Boettger@gmail.com>
2021-03-20 15:06:31 -0400

    blah:

--- algo_prac.py
+++ algo_prac.py
@@ -1,7 +1,26 @@
-def unique_in_order(iterable):
-    able = list(iterable.split(' '))
-    new_iterable = []
-    for i in range(len(iterable)):
+
+def strStr(haystack, needle):
+    end = len(haystack)-1
+    beg = 0
+    leng = len(needle)-1
+    if leng == 0:
+        return 0
+
+    while end > beg:
+        if haystack[end] == needle[-1]:
+            if haystack[(end-leng):end] == needle:
+                return end-leng
+            elif haystack[beg] == needle[0]:
+                if haystack[beg: (beg+leng)] == needle:
+                    return beg
+            else:
+                beg = beg+1
+                end = end-1
+                print(beg)
+                print(end)
+        return -1
 
 
-unique_in_order('AAAABBBCCDAABBB')
+print(strStr("hello", "ll"))
+
+str = 'tomb'
