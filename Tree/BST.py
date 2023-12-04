d409c743e8e75ebdaa60319967ee55c496480471
Tbetcha <troy.boettger@gmail.com>
2023-12-03 23:14:33 -0500

    ok

--- Tree/BST.py
+++ Tree/BST.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.left_child = None
         self.right_child = None
@@ -21,10 +21,9 @@ def insert_node(root_node, node_value):
     return "The node has been inserted"
 
 
-
 def pre_order(root_node):
     if not root_node:
-        return 
+        return
     print(root_node.data)
     pre_order(root_node.left_child)
     pre_order(root_node.right_child)
@@ -37,9 +36,10 @@ def in_order(root_node):
     print(root_node.data)
     in_order(root_node.right_child)
 
+
 def post_order(root_node):
     if not root_node:
-        return 
+        return
     post_order(root_node.left_child)
     post_order(root_node.right_child)
     print(root_node.data)
@@ -47,7 +47,7 @@ def post_order(root_node):
 
 def search(root_node, node_value):
     if root_node.data == node_value:
-        print('The value was found')
+        print("The value was found")
     elif node_value < root_node.data:
         if root_node.left_child.data == node_value:
             print("the value is found")
@@ -55,8 +55,9 @@ def search(root_node, node_value):
             search(root_node.left_child, node_value)
     else:
         if root_node.right_child.data == node_value:
-            print('the value is found')
-        else: return search(root_node.right_child, node_value)
+            print("the value is found")
+        else:
+            return search(root_node.right_child, node_value)
 
 
 # delete has three cases one child, two child, leaf
@@ -66,7 +67,7 @@ def search(root_node, node_value):
 # successor will always be min value in right subtree
 def min_val(node):
     current = node
-    while(current.left_child is not None):
+    while current.left_child is not None:
         current = current.left_child
     return current
 
@@ -74,11 +75,11 @@ def min_val(node):
 def del_node(root_node, node_value):
     if root_node is None:
         return root_node
-    #find the node and has no children
+    # find the node and has no children
     if node_value < root_node.data:
         root_node.left_child = del_node(root_node.left_child, node_value)
     elif node_value > root_node.data:
-        root_node.right_child= del_node(root_node.right_child, node_value)
+        root_node.right_child = del_node(root_node.right_child, node_value)
     else:
         # if node has one child
         if root_node.left_child is None:
@@ -89,10 +90,10 @@ def del_node(root_node, node_value):
             temp = root_node.left_child
             root_node = None
             return temp
-        #if node has two children swap w/ successor then delete successor
+        # if node has two children swap w/ successor then delete successor
         temp = min_val(root_node.right_child)
         root_node.data = temp.data
-        root_node.right_child  = del_node(root_node.right_child, temp.data)
+        root_node.right_child = del_node(root_node.right_child, temp.data)
     return root_node
 
 
@@ -103,6 +104,7 @@ def del_tree(root_node):
     root_node.left_child = None
     root_node.right_child = None
     return "The tree has been deleted"
+
 
 new_tree = Node(None)
 print(insert_node(new_tree, 70))
@@ -117,13 +119,3 @@ search(new_tree, 60)
 del_node(new_tree, 10)
 print(in_order(new_tree))
 print(del_tree(new_tree))
-
-
-
-
-
-
-
-
-
-
